1ad1e020074008513a1db0f46ee3b06e74d37abb
Chukwuemeka Abuba <abubachukwuemeka@outlook.com>
2023-01-16 00:50:58 +0100

    create the header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="text-red-300">
-      Hello
-    </div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/offers" element={<Offers/>} />
+        </Routes>
+      </Router>
+    </>
   );
+
 }
 
 export default App;
+
